3c2e90d822297fb691d50bef7aa70d14d77fd7f5
Aigul <aigul.sibgatulli@mail.ru>
2021-01-17 13:42:42 +0300

    routing

---
+++ src/components/pages/home-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookList from '../book-list';
+
+const HomePage = () => {
+    return (
+        <>
+            <div>Home Page</div>
+            <BookList books={[]}/>
+        </>
+    );
+};
+
+export default HomePage;
