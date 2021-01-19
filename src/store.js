26eed83d26a0610bc5ca11bc74254f58386418b2
Aigul <aigul.sibgatulli@mail.ru>
2021-01-19 20:42:42 +0300

    adding enhancers

--- src/store.js
+++ src/store.js
@@ -1,6 +1,50 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            });
+        }
+
+        originalDispatch(action);
+    };
+
+    return store;
+};
+
+// enhancers already exist in community
+const logEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        console.log(action.type);
+        return originalDispatch(action);
+    };
+
+    return store;
+};
+
+// const store = createStore(reducer);
+// const store = createStore(reducer, logEnhancer);
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer)); // first, second
+
+store.dispatch('ACTION');
+
+// // monkey patching, if library does not support any extensions
+// const originalDispatch = store.dispatch;
+// store.dispatch = (action) => {
+//     if (typeof action === 'string') {
+//         return originalDispatch({
+//             type: action
+//         });
+//     }
+
+//     originalDispatch(action);
+// };
 
 export default store;
